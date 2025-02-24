952bfe3f9a57c1c25182b1c4b82b3d62e26a33ca
Prajna Agrawal <147647745+PrajnaAgrawal26@users.noreply.github.com>
2025-02-24 10:45:43 +0530

    Update model.py

--- model.py
+++ model.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torch.autograd import Variable
 import numpy as np
 from PIL import Image
 
@@ -57,12 +56,12 @@ class iCaRLNet(nn.Module):
         out_features = self.fc.out_features
         weight = self.fc.weight.data
 
-        self.fc = nn.Linear(in_features, out_features+n, bias=False)
+        self.fc = nn.Linear(in_features, out_features + n, bias=False)
         self.fc.weight.data[:out_features] = weight
         self.n_classes += n
 
     def classify(self, x, transform):
-        """Classify images by neares-means-of-exemplars
+        """Classify images by nearest-means-of-exemplars
 
         Args:
             x: input image batch
@@ -72,41 +71,40 @@ class iCaRLNet(nn.Module):
         batch_size = x.size(0)
 
         if self.compute_means:
-            print ("Computing mean of exemplars...")
+            print("Computing mean of exemplars...")
             exemplar_means = []
             for P_y in self.exemplar_sets:
                 features = []
                 # Extract feature for each exemplar in P_y
                 for ex in P_y:
-                    ex = Variable(transform(Image.fromarray(ex)), volatile=True).cuda()
-                    feature = self.feature_extractor(ex.unsqueeze(0))
-                    feature = feature.squeeze()
-                    feature.data = feature.data / feature.data.norm() # Normalize
-                    features.append(feature)
+                    with torch.no_grad():
+                        ex = transform(Image.fromarray(ex)).cuda()
+                        feature = self.feature_extractor(ex.unsqueeze(0))
+                        feature = feature.squeeze()
+                        feature = feature / feature.norm()  # Normalize
+                        features.append(feature)
                 features = torch.stack(features)
                 mu_y = features.mean(0).squeeze()
-                mu_y.data = mu_y.data / mu_y.data.norm() # Normalize
+                mu_y = mu_y / mu_y.norm()  # Normalize
                 exemplar_means.append(mu_y)
             self.exemplar_means = exemplar_means
             self.compute_means = False
             print("Done")
 
         exemplar_means = self.exemplar_means
-        means = torch.stack(exemplar_means) # (n_classes, feature_size)
-        means = torch.stack([means] * batch_size) # (batch_size, n_classes, feature_size)
-        means = means.transpose(1, 2) # (batch_size, feature_size, n_classes)
+        means = torch.stack(exemplar_means)  # (n_classes, feature_size)
+        means = torch.stack([means] * batch_size)  # (batch_size, n_classes, feature_size)
+        means = means.transpose(1, 2)  # (batch_size, feature_size, n_classes)
 
-        feature = self.feature_extractor(x) # (batch_size, feature_size)
-        for i in range(feature.size(0)): # Normalize
-            feature.data[i] = feature.data[i] / feature.data[i].norm()
-        feature = feature.unsqueeze(2) # (batch_size, feature_size, 1)
-        feature = feature.expand_as(means) # (batch_size, feature_size, n_classes)
+        feature = self.feature_extractor(x)  # (batch_size, feature_size)
+        feature = feature / feature.norm(dim=1, keepdim=True)  # Normalize
+        feature = feature.unsqueeze(2)  # (batch_size, feature_size, 1)
+        feature = feature.expand_as(means)  # (batch_size, feature_size, n_classes)
 
-        dists = (feature - means).pow(2).sum(1).squeeze() #(batch_size, n_classes)
+        dists = (feature - means).pow(2).sum(1).squeeze()  # (batch_size, n_classes)
         _, preds = dists.min(1)
 
         return preds
-        
 
     def construct_exemplar_set(self, images, m, transform):
         """Construct an exemplar set for image set
@@ -117,41 +115,34 @@ class iCaRLNet(nn.Module):
         # Compute and cache features for each example
         features = []
         for img in images:
-            x = Variable(transform(Image.fromarray(img)), volatile=True).cuda()
-            feature = self.feature_extractor(x.unsqueeze(0)).data.cpu().numpy()
-            feature = feature / np.linalg.norm(feature) # Normalize
-            features.append(feature[0])
+            with torch.no_grad():
+                x = transform(Image.fromarray(img)).cuda()
+                feature = self.feature_extractor(x.unsqueeze(0)).cpu().numpy()
+                feature = feature / np.linalg.norm(feature)  # Normalize
+                features.append(feature[0])
 
         features = np.array(features)
         class_mean = np.mean(features, axis=0)
-        class_mean = class_mean / np.linalg.norm(class_mean) # Normalize
+        class_mean = class_mean / np.linalg.norm(class_mean)  # Normalize
 
         exemplar_set = []
-        exemplar_features = [] # list of Variables of shape (feature_size,)
+        exemplar_features = []  # list of Variables of shape (feature_size,)
         for k in range(m):
             S = np.sum(exemplar_features, axis=0)
             phi = features
             mu = class_mean
-            mu_p = 1.0/(k+1) * (phi + S)
+            mu_p = 1.0 / (k + 1) * (phi + S)
             mu_p = mu_p / np.linalg.norm(mu_p)
-            i = np.argmin(np.sqrt(np.sum((mu - mu_p) ** 2, axis=1)))
+            i = np.argmin(np.linalg.norm(mu - mu_p, axis=1))
 
             exemplar_set.append(images[i])
             exemplar_features.append(features[i])
-            """
-            print "Selected example", i
-            print "|exemplar_mean - class_mean|:",
-            print np.linalg.norm((np.mean(exemplar_features, axis=0) - class_mean))
-            #features = np.delete(features, i, axis=0)
-            """
-        
+
         self.exemplar_sets.append(np.array(exemplar_set))
-                
 
     def reduce_exemplar_sets(self, m):
         for y, P_y in enumerate(self.exemplar_sets):
             self.exemplar_sets[y] = P_y[:m]
-
 
     def combine_dataset_with_exemplars(self, dataset):
         for y, P_y in enumerate(self.exemplar_sets):
@@ -159,9 +150,7 @@ class iCaRLNet(nn.Module):
             exemplar_labels = [y] * len(P_y)
             dataset.append(exemplar_images, exemplar_labels)
 
-
     def update_representation(self, dataset):
-
         self.compute_means = True
 
         # Increment number of weights in final fc layer
@@ -169,51 +158,50 @@ class iCaRLNet(nn.Module):
         new_classes = [cls for cls in classes if cls > self.n_classes - 1]
         self.increment_classes(len(new_classes))
         self.cuda()
-        print ("%d new classes" % (len(new_classes)))
+        print(f"{len(new_classes)} new classes")
 
         # Form combined training set
         self.combine_dataset_with_exemplars(dataset)
 
         loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
-                                               shuffle=True, num_workers=2)
+                                             shuffle=True, num_workers=2)
 
         # Store network outputs with pre-update parameters
         q = torch.zeros(len(dataset), self.n_classes).cuda()
         for indices, images, labels in loader:
-            images = Variable(images).cuda()
+            images = images.cuda()
             indices = indices.cuda()
-            g = F.sigmoid(self.forward(images))
-            q[indices] = g.data
-        q = Variable(q).cuda()
+            with torch.no_grad():
+                g = torch.sigmoid(self.forward(images))
+            q[indices] = g
+
+        q = q.cuda()
 
         # Run network training
         optimizer = self.optimizer
 
         for epoch in range(num_epochs):
             for i, (indices, images, labels) in enumerate(loader):
-                images = Variable(images).cuda()
-                labels = Variable(labels).cuda()
+                images = images.cuda()
+                labels = labels.cuda()
                 indices = indices.cuda()
 
                 optimizer.zero_grad()
                 g = self.forward(images)
-                
+
                 # Classification loss for new classes
                 loss = self.cls_loss(g, labels)
-                #loss = loss / len(range(self.n_known, self.n_classes))
 
-                # Distilation loss for old classes
+                # Distillation loss for old classes
                 if self.n_known > 0:
-                    g = F.sigmoid(g)
+                    g = torch.sigmoid(g)
                     q_i = q[indices]
-                    dist_loss = sum(self.dist_loss(g[:,y], q_i[:,y])\
-                            for y in range(self.n_known))
-                    #dist_loss = dist_loss / self.n_known
+                    dist_loss = sum(self.dist_loss(g[:, y], q_i[:, y])
+                                    for y in range(self.n_known))
                     loss += dist_loss
 
                 loss.backward()
                 optimizer.step()
 
-                if (i+1) % 10 == 0:
-                    print ('Epoch [%d/%d], Iter [%d/%d] Loss: %.4f' 
-                           %(epoch+1, num_epochs, i+1, len(dataset)//batch_size, loss.data[0]))
+                if (i + 1) % 10 == 0:
+                    print(f'Epoch [{epoch + 1}/{num_epochs}], Iter [{i + 1}/{len(dataset) // batch_size}] Loss: {loss.item():.4f}')
